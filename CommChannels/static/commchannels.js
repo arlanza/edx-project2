36b3e86ce16d20cab21ea05d9d040c3b7a75f814
Laura Alonso <laura.alonsoibanez@gmail.com>
2020-04-25 19:57:52 +0200

    Login

--- CommChannels/static/commchannels.js
+++ CommChannels/static/commchannels.js
@@ -1,19 +1,39 @@
-// Set starting value of counter to 0
-if (!localStorage.getItem('counterlai'))
-    localStorage.setItem('counterlai', 0);
 
-// Load current value of  counter
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#counter').innerHTML = localStorage.getItem('counter');
 
-    // Count every time button is clicked
-    document.querySelector('button').onclick = () => {
-        // Increment current counter
-        let counter = localStorage.getItem('counterlai');
-        counter++;
+  // Login firt time access
+  if (!localStorage.getItem('loginname')){
 
-        // Update counter
-        document.querySelector('#counter').innerHTML = counter;
-        localStorage.setItem('counterlai', counter);
+    //First time access
+
+    //Form to login visible
+    document.querySelector('#layout').querySelector("#form-loginname").style.visibility = "visible";
+
+    // By default, submit button is disabled. Enable if there's text name
+    document.querySelector('#submit-loginname').disabled = true;
+
+    document.querySelector('#text-loginname').onkeyup = () => {
+        if (document.querySelector('#name').value.length > 0)
+          document.querySelector('#submit-loginname').disabled = false;
+        else
+          document.querySelector('#submit-loginname').disabled = true;
     }
+
+    // Get text-loginname and set at localStorage
+    document.querySelector('#layout').querySelector("#form-loginname").onsubmit = () => {
+        alert("Submit - Tengo nombre");
+        localStorage.setItem('loginname', document.querySelector('#text-loginname').value);
+
+        document.querySelector('#layout').querySelector("#form-loginname").style.visibility = "hidden";
+
+    }
+  } else {
+    // It's not first time access
+    document.querySelector('#layout').querySelector("#form-loginname").style.visibility = "hidden";
+  }
+
+  // Display the loginname
+  document.querySelector('#loginname').innerHTML = localStorage.getItem('loginname');
+
+
 });
